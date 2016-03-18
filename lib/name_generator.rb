2111dc7933a50a5b70cb72b4eb4c4703bf208c88
Jay Bernal <jjbernal@gmail.com>
2016-03-18 08:58:52 -0500

    still not working

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -1,4 +1,27 @@
 # You'll have to create your own method(s) here and define them. Use patterns like
 # you've seen elsewhere.
 
-require 'pry'
+
+def myspace_name_generator(name)
+    myspace_name = join_first_last_name(first, last)
+    myspace_name = deleting_all_char(first, last)
+end
+
+def join_first_last_name(name)
+    joining = first + last
+end
+
+def deleting_all_char(name)
+	joining_name = joining_name.gsub!(/[^0-9A-Za-z]/, '')
+end
+
+def upcase_downcase(name)
+	name.each_with_index do |letter, index|
+    if index % 2 == 0
+       name[index].upcase
+       elsif 
+       name[index].downcase
+    end
+end
+
+myspace_name_generator("Jay bernal")
